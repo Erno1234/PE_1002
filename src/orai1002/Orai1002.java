20765182fbc9616fea0045e5794aa96efe610303
Erno1234 <palocierno12@gmail.com>
2023-10-02 13:00:32 +0200

    Kezdés

---
+++ src/orai1002/Orai1002.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package orai1002;
+
+/**
+ *
+ * @author paloci.erno
+ */
+public class Orai1002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
